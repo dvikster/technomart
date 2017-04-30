6231f9e91e03d472086a69c71dad465f3940c3c7
dvikster <dvikster@gmail.com>
2017-04-30 22:09:52 +0300

    added js for slider, products and services

---
+++ js/common.js
@@ -0,0 +1,94 @@
+
+//slider
+var stepMove = 0;
+var aroowLeft = document.getElementById("arrow_left");
+var aroowRight = document.getElementById("arrow_right");
+var moveSlider = document.getElementsByClassName("slider")[0];
+
+aroowLeft.onclick =  function(){
+    event.preventDefault();
+    if( stepMove == 0){
+
+    }
+    else{
+        stepMove = stepMove + 620;
+        pagination();
+    }
+    moveSlider.style.marginLeft = stepMove + "px";
+}
+aroowRight.onclick =  function(){
+    event.preventDefault();
+    if( stepMove == -1860){
+
+    }
+    else{
+        stepMove = stepMove - 620;
+        pagination();
+    }
+    moveSlider.style.marginLeft = stepMove + "px";
+}
+
+
+
+
+// как это уменьшить и сделать правильно - и это не пагинация, а индикация слайдеров
+function pagination(){
+    if(stepMove == 0 ){
+        document.getElementsByClassName("pagin")[1].classList.remove("active");
+        document.getElementsByClassName("pagin")[2].classList.remove("active");
+        document.getElementsByClassName("pagin")[3].classList.remove("active");
+        document.getElementsByClassName("pagin")[0].classList.add("active");
+    }
+    else if(stepMove ==  -620){
+        document.getElementsByClassName("pagin")[0].classList.remove("active");
+        document.getElementsByClassName("pagin")[2].classList.remove("active");
+        document.getElementsByClassName("pagin")[3].classList.remove("active");
+        document.getElementsByClassName("pagin")[1].classList.add("active");
+    }
+    else if(stepMove == -1240){
+        document.getElementsByClassName("pagin")[0].classList.remove("active");
+        document.getElementsByClassName("pagin")[1].classList.remove("active");
+        document.getElementsByClassName("pagin")[3].classList.remove("active");
+        document.getElementsByClassName("pagin")[2].classList.add("active");
+    }
+    else if(stepMove == -1860){
+        document.getElementsByClassName("pagin")[0].classList.remove("active");
+        document.getElementsByClassName("pagin")[1].classList.remove("active");
+        document.getElementsByClassName("pagin")[2].classList.remove("active");
+        document.getElementsByClassName("pagin")[3].classList.add("active");
+    }
+}
+
+// - по аналогии как в крестиках ноликах, должен біть вариант по проще
+function hover_product(id, hover) {
+    document.getElementById(id).className = 'col-3 product_item box ' + hover;
+}
+
+function out_product(id, out){
+    document.getElementById(id).className = 'col-3 product_item box ' + out;
+}
+
+
+// -  нашла такой код - было интересно испробывать для табов https://www.w3schools.com/howto/howto_js_tabs.asp
+function openServices(evt, servName) {
+    event.preventDefault();
+
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(servName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
