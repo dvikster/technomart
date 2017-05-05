052797d4ad41f2f7f5ff9271a4da632e26380ea9
dvikster <dvikster@gmail.com>
2017-05-05 10:51:42 +0300

    optimization js - for tabs

--- js/common.js
+++ js/common.js
@@ -58,9 +58,9 @@ function pagination (){
 // смена блока на ховер для продуктов
 var product= document.getElementsByClassName('col-3 product_item box');
 
-for(var i=0; i<product.length; i++){
-    product[i].onmouseover = productHover;
-    product[i].onmouseout = productOut;
+for(var a=0; a<product.length; a++){
+    product[a].onmouseover = productHover;
+    product[a].onmouseout = productOut;
 }
 function productHover(){
     this.className = 'col-3 product_item box hover';
@@ -69,31 +69,49 @@ function productOut(){
     this.className = 'col-3 product_item box out';
 }
 
+// tabs whis data-tab
+var tablinks = document.getElementsByClassName('tablinks');
+var tabcontent = document.getElementsByClassName('tabcontent');
 
-
-// -  нашла такой код - было интересно испробывать для табов https://www.w3schools.com/howto/howto_js_tabs.asp
-function openServices(event, servName) {
-    event.preventDefault();
-
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(servName).style.display = "block";
-    event.currentTarget.className += " active";
+for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].onclick = tabChange;
 }
+
+function tabChange() {
+    event.preventDefault();
+    for (var a = 0; a < tabcontent.length; a++) {
+        tabcontent[a].style.display = "none";
+    }
+    var dataTab= this.getAttribute('data-tab');
+    document.querySelector('.tabcontent[data-tab="' + dataTab + '"]').style.display = 'block';
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove('active');
+    }
+    this.classList.add('active');
+}
+
+tablinks[0].click();
+
+
+// // -  нашла такой код - было интересно испробывать для табов https://www.w3schools.com/howto/howto_js_tabs.asp
+// function openServices(event, servName) {
+//     event.preventDefault();
+//     // Declare all variables
+//     var i, tabcontent, tablinks;
+//     // Get all elements with class="tabcontent" and hide them
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+//     // Get all elements with class="tablinks" and remove the class "active"
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     // Show the current tab, and add an "active" class to the button that opened the tab
+//     document.getElementById(servName).style.display = "block";
+//     event.currentTarget.className += " active";
+// }
 
 
 document.getElementById('map_popup').onclick = function (event) {
